6b2f67fdd171b143edf4c88b9eebf12ab29bf593
labcabrera <lab.cabrera@gmail.com>
2024-09-13 12:28:18 +0200

    Initial version.

--- src/app.js
+++ src/app.js
@@ -5,14 +5,14 @@ const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
-const dbURI = process.env.MONGO_URI || 'mongodb://localhost:27017/rmu-tactical';
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/rmu-tactical';
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Conectado a MongoDB'))
-  .catch((err) => console.log('Error conectando a MongoDB', err));
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to ' + MONGO_URI))
+  .catch((err) => console.log('Error connecting to ' + MONGO_URI, err));
 
 const usersRouter = require('./routes/tactical-games');
 app.use('/v1/tactical-games', usersRouter);
--- src/routes/tactical-games.js
+++ src/routes/tactical-games.js
@@ -3,34 +3,6 @@ const { MongoClient } = require('mongodb');
 const router = express.Router();
 const TacticalGame = require("../models/tactical-game")
 
-let games = [
-    {
-        id: 1,
-        name: 'La batalla de Arnor',
-        description: 'Lorem ipsum',
-        status: 'open',
-        username: 'lab.cabrera@gmail.com',
-        created: '2024-09-12T19:20:15'
-    },
-    {
-        id: 2,
-        name: 'Modo Arena Testing',
-        description: 'Lorem ipsum',
-        status: 'closed',
-        username: 'lab.cabrera@gmail.com',
-        status: 'open',
-        created: '2024-09-12T19:20:15'
-    },
-    {
-        id: 3,
-        name: 'Campaña LOTR - Batalla Mordor - 1',
-        description: 'Lorem ipsum',
-        status: 'open',
-        username: 'lab.cabrera@gmail.com',
-        created: '2024-09-12T19:20:15'
-    }
-];
-
 router.get('/', async (req, res) => {
     try {
         const games = await TacticalGame.find();
@@ -68,7 +40,7 @@ router.put('/:id', async (req, res) => {
     try {
         const updatedGame = await TacticalGame.findByIdAndUpdate(req.params.id, req.body, { new: true });
         if (!updatedGame) {
-            return res.status(404).json({ message: 'Item no encontrado' });
+            return res.status(404).json({ message: 'Tactical game not found' });
         }
         res.json(updatedItem);
     } catch (error) {
@@ -81,8 +53,8 @@ router.delete('/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const deletedGame = await TacticalGame.findByIdAndDelete(id);
-        if (!deletedGame) return res.status(404).json({ message: 'Item no encontrado' });
-        res.json({ message: 'Deleted game ' + id });
+        if (!deletedGame) return res.status(404).json({ message: 'Tactical game not found' });
+        res.json({ message: 'Deleted tactical game ' + id });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
