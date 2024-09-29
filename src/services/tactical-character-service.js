b553ac2c474913b3a32a6371120bf60ed2e82d7e
labcabrera <lab.cabrera@gmail.com>
2024-09-29 22:26:26 +0200

    Initial character update.

--- src/services/tactical-character-service.js
+++ src/services/tactical-character-service.js
@@ -59,10 +59,14 @@ const insert = async (user, data) => {
 };
 
 const update = async (characterId, data) => {
-    const { name, description } = data;
-    const updatedCharacter = await TacticalCharacter.findByIdAndUpdate(characterId, { name, description }, { new: true });
+    const current = await TacticalCharacter.findById(characterId);
+    if (!current) {
+        throw { state: 404, message: 'Tactical character not found' };
+    }
+    const update = buildCharacterUpdate(data, current);
+    const updatedCharacter = await TacticalCharacter.findByIdAndUpdate(characterId, update, { new: true });
     if (!updatedCharacter) {
-        throw { status: 404, message: "Tactical character not found" };
+        throw { status: 404, message: 'Tactical character not found' };
     };
     return tacticalCharacterConverter.toJSON(updatedCharacter);
 };
@@ -107,6 +111,75 @@ const setCurrentHp = async (characterId, hp) => {
         { hp: { max: maxHp, current: hp } },
         { new: true });
     return toJSON(updatedCharacter);
+};
+
+const buildCharacterUpdate = (data, currentCharacter) => {
+    const update = {};
+    if (data.name) {
+        update.name = data.name;
+    }
+    if (data.faction) {
+        update.faction = data.faction;
+    }
+    if(data.description) {
+        update.description = data.description;
+    }
+    buildCharacterUpdateInfo(update, data, currentCharacter);
+    buildCharacterUpdateDefense(update, data, currentCharacter);
+    buildCharacterUpdateHp(update, data, currentCharacter);
+    buildCharacterUpdateInitiative(update, data, currentCharacter);
+    return update;
+};
+
+const buildCharacterUpdateInfo = (update, data, currentCharacter) => {
+    if (data.info) {
+        update.info = currentCharacter.info;
+        if (data.info.level) {
+            update.info.level = data.info.level;
+        }
+        if (data.info.race) {
+            update.info.race = data.info.race;
+        }
+        if(data.info.sizeId) {
+            update.info.sizeId = data.info.sizeId;
+        }
+        if(data.info.baseMovementRate) {
+            update.info.baseMovementRate = data.info.baseMovementRate;
+        }
+    }
+};
+
+const buildCharacterUpdateDefense = (update, data, currentCharacter) => {
+    if (data.defense) {
+        update.defense = currentCharacter.defense;
+        if (data.defense.armorType) {
+            update.defense.armorType = data.defense.armorType;
+        }
+        if (data.defense.defensiveBonus) {
+            update.defense.defensiveBonus = data.defense.defensiveBonus;
+        }
+    }
+};
+
+const buildCharacterUpdateHp = (update, data, currentCharacter) => {
+    if (data.hp) {
+        update.hp = currentCharacter.hp;
+        if (data.hp.max) {
+            update.hp.max = data.hp.max;
+        }
+        if (data.hp.current) {
+            update.hp.current = data.hp.current;
+        }
+    }
+};
+
+const buildCharacterUpdateInitiative = (update, data, currentCharacter) => {
+    if (data.initiative) {
+        update.initiative = currentCharacter.initiative;
+        if (data.initiative.base) {
+            update.initiative.base = data.initiative.base;
+        }
+    }
 };
 
 const loadDefaultEquipment = async (character) => {
