1b10cb1468dff0b9c95fd9399c61738bcb024fe4
labcabrera <lab.cabrera@gmail.com>
2024-10-15 12:54:05 +0200

    Update attack service.

--- src/services/attack/attack-bonus-processor.js
+++ src/services/attack/attack-bonus-processor.js
@@ -14,8 +14,17 @@ const process = (action, bonus) => {
 };
 
 const restrictedQuartersProcessor = (action) => {
-    //TODO
-    return -5;
+    if (!action.attackInfo.restrictedQuarters) {
+        return 0;
+    }
+    switch (action.attackInfo.restrictedQuarters) {
+        case 'none': return 0;
+        case 'close': return -25;
+        case 'cramped': return -50;
+        case 'tigth': return -75;
+        case 'confined': return -100;
+        default: throw { status: 400, message: 'Invalid restricted quarters value' };
+    }
 }
 
 module.exports = {
