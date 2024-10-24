15cb0ec69a3c82edfdce0245d830f87d6470e6ff
labcabrera <lab.cabrera@gmail.com>
2024-10-24 18:24:59 +0200

    Item equip refactor

--- src/services/items/item-equip-service.js
+++ src/services/items/item-equip-service.js
@@ -4,12 +4,50 @@ const tacticalCharacterConverter = require('../../converters/tactical-character-
 const equip = async (characterId, data) => {
     if (!data.itemId) throw { status: 400, message: 'Required itemId' };
 
-    const currentCharacter = await TacticalCharacter.findById(characterId);
+    const character = await TacticalCharacter.findById(characterId);
+    validateEquipData(character, data);
+
     const itemId = data.itemId;
     const slot = data.slot;
+    const item = character.items.find(e => e.id == itemId);
 
+    if (slot === 'body' && item.armor && item.armor.armorType) {
+        character.defense.armorType = item.armor.armorType;
+    }
+    const slots = ['mainHand', 'offHand', 'body', 'head', 'arms', 'legs'];
+    for (const checkSlot of slots) {
+        if (character.equipment[checkSlot] == itemId) {
+            character.equipment[checkSlot] = null;
+        }
+    }
+    if (slot) {
+        character.equipment[slot] = itemId;
+    }
+    if (slot && slot === 'mainHand' && item.weapon && item.weapon.requiredHands > 1) {
+        character.equipment.offHand = null;
+    }
+    if (slot && slot === 'offHand' && item.weapon && item.weapon.requiredHands > 1) {
+        throw { status: 400, message: 'Two handed weapons cant be equiped in offHand slot' };
+    }
+    if(slot && slot === 'offHand' && character.equipment.mainHand) {
+        const mainHandItem = character.items.find(e => e.id == character.equipment.mainHand);
+        if(mainHandItem.weapon && mainHandItem.weapon.requiredHands > 1) {
+            character.equipment.mainHand = null;
+        }
+    }
+    // No armor equiped
+    if (character.equipment.body === null) {
+        character.defense.armorType = 1;
+    }
+
+    await character.save();
+    return tacticalCharacterConverter.toJSON(character);
+};
+
+const validateEquipData = (currentCharacter, data) => {
     if (!currentCharacter) throw { status: 404, message: 'Tactical character not found' };
-
+    const itemId = data.itemId;
+    const slot = data.slot;
     const item = currentCharacter.items.find(e => e.id == itemId);
     if (!item) throw { status: 400, message: 'Invalid itemId' };
 
@@ -33,46 +71,6 @@ const equip = async (characterId, data) => {
                 throw { status: 400, message: 'Invalid item slot' };
         }
     }
-    const update = {
-        equipment: currentCharacter.equipment
-    };
-    // Update equiped armor type
-    if (slot === 'body' && item.armor && item.armor.armorType) {
-        update.defense = currentCharacter.defense;
-        update.defense.armorType = item.armor.armorType;
-    }
-    const slots = ['mainHand', 'offHand', 'body', 'head', 'arms', 'legs'];
-    for (const checkSlot of slots) {
-        if (update.equipment[checkSlot] == itemId) {
-            update.equipment[checkSlot] = null;
-        }
-    }
-    if (slot) {
-        update.equipment[slot] = itemId;
-    }
-    if (slot && slot === 'mainHand' && item.weapon && item.weapon.requiredHands > 1) {
-        update.equipment.offHand = null;
-    }
-    if (slot && slot === 'offHand' && item.weapon && item.weapon.requiredHands > 1) {
-        throw { status: 400, message: 'Two handed weapons cant be equiped in offHand slot' };
-    }
-    // if an offHand item is equipped and you have a two-handed weapon, it is unequipped.
-    if(slot && slot === 'offHand' && currentCharacter.equipment.mainHand) {
-        const mainHandItem = currentCharacter.items.find(e => e.id == currentCharacter.equipment.mainHand);
-        if(mainHandItem.weapon && mainHandItem.weapon.requiredHands > 1) {
-            update.equipment.mainHand = null;
-        }
-    }
-    // No armor equiped
-    if (update.equipment.body === null) {
-        update.defense = currentCharacter.defense;
-        update.defense.armorType = 1;
-    }
-    const updated = await TacticalCharacter.findOneAndUpdate({ _id: characterId }, update, {
-        new: true,
-        upsert: true
-    });
-    return tacticalCharacterConverter.toJSON(updated);
 };
 
 module.exports = {
