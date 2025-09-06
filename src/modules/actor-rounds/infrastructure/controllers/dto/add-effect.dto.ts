336dc01ba21eda7696c4ddf864e9696a4ef3f9ff
labcabrera <lab.cabrera@gmail.com>
2025-09-07 00:39:19 +0200

    Fix dto.

--- src/modules/actor-rounds/infrastructure/controllers/dto/add-effect.dto.ts
+++ src/modules/actor-rounds/infrastructure/controllers/dto/add-effect.dto.ts
@@ -1,9 +1,17 @@
+import { IsNumber, IsOptional, IsString } from 'class-validator';
 import { AddEffectCommand } from '../../../application/commands/add-effect.command';
 import { ActorRoundEffect } from '../../persistence/models/actor-round.models-childs';
 
 export class AddEffectDto {
+  @IsString()
   status: string;
+
+  @IsOptional()
+  @IsNumber()
   value: number | undefined;
+
+  @IsOptional()
+  @IsNumber()
   rounds: number | undefined;
 
   static toCommand(id: string, dto: AddEffectDto, userId: string, userRoles: string[]): AddEffectCommand {
--- src/modules/actor-rounds/infrastructure/controllers/dto/add-hp.dto.ts
+++ src/modules/actor-rounds/infrastructure/controllers/dto/add-hp.dto.ts
@@ -1,6 +1,8 @@
+import { IsNumber } from 'class-validator';
 import { AddHpCommand } from '../../../application/commands/add-hp.command';
 
 export class AddHpDto {
+  @IsNumber()
   hp: number;
 
   static toCommand(id: string, dto: AddHpDto, userId: string, userRoles: string[]): AddHpCommand {
